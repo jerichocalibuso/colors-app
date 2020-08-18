5f455f2370b049ba1c923a1212042b44cb434f67
jerichocalibuso <jerichocalibuso@gmail.com>
2020-08-18 11:58:51 +0800

    Add ColorBox

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import seedColors from "./seedColors";
 function App() {
   return (
     <div className="">
-      <Palette {...seedColors[4]} />
+      <Palette {...seedColors[0]} />
     </div>
   );
 }
